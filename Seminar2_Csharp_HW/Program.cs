1b77de836b6a1f9879796ab9c5a20b0ad43458a3
Darya_Kuptsova <darya.kuptsova.prof@gmail.com>
2022-12-04 22:26:43 +0300

    First task added (wo check for 3 digits)

--- Seminar2_Csharp_HW/Program.cs
+++ Seminar2_Csharp_HW/Program.cs
@@ -1,2 +1,59 @@
-﻿// Задача
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+int SecondDigit(int Num)
+{
+    int second = Num / 10 % 10;
+    return second;
+}
 
+Console.WriteLine("Enter a three digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int newnum = SecondDigit(num);
+Console.WriteLine("Second number is: " + newnum);
+
+
+if (a / 100 > 0 && a / 1000 == 0)
+
+// Попытки добавить проверку таким образом, чтобы снова запускалось сообщение о необходимости ввода числа, не увенчались успехом.
+// Ниже оставила свои варианты (это не одна программа, а несколько кусочков вариантов).
+
+// if (a / 100 > 0 && a / 1000 == 0)
+
+// int Check(int a)
+// {
+//     if (a > 999 || a < 100)
+//         Console.WriteLine("Вы ввели не трехзначное число. Введите число из трех знаков");
+//     return a;
+// }
+// int result = Check(num);
+// Console.WriteLine(result);
+
+// while (num < 1000 && num > 99)
+// {
+//     Console.WriteLine("Enter a three digit number: ");
+//     num = Convert.ToInt32(Console.ReadLine());
+//     int newnum = SecondDigit(num);
+//     Console.WriteLine("Second number is: " + newnum);
+// }
+// Console.WriteLine("Вы ввели не трехзначное число. Введите число из трех знаков");
+
+
+// if (Num > 999 || Num < 100)
+//         Console.WriteLine("Вы ввели не трехзначное число. Введите число из трех знаков");
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+*/
+int ThirdDigit (int num)
+
+
+
+
+
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+*/
