b1b37fa57e4555f0c7f65621631a46cd4f81ec26
Darya_Kuptsova <darya.kuptsova.prof@gmail.com>
2022-12-05 14:15:57 +0300

    Seminar2 HW done

--- Seminar2_Csharp_HW/Program.cs
+++ Seminar2_Csharp_HW/Program.cs
@@ -1,24 +1,26 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-/*
+// Добавить проверку на то, трехзначное число или нет внутрь метода тоже получилось (по типу второй задачи), но избавиться от неверного вывода второй цифры при этом не вышло. Поэтому отсавляю проверку вне метода.
+
 int SecondDigit(int Num)
 {
     int second = Num / 10 % 10;
     return second;
 }
 
-Console.WriteLine("Enter a three digit number: ");
+Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+if (num > 999 || num < 100)
+    Console.WriteLine("Вы ввели не трехзначное число!");
+else
+    {
+    int newnum = SecondDigit(num);
+    Console.WriteLine("Вторая цифра этого числа: " + newnum);
+    }
 
-int newnum = SecondDigit(num);
-Console.WriteLine("Second number is: " + newnum);
-
-
-if (a / 100 > 0 && a / 1000 == 0)
 
 // Попытки добавить проверку таким образом, чтобы снова запускалось сообщение о необходимости ввода числа, не увенчались успехом.
 // Ниже оставила свои варианты (это не одна программа, а несколько кусочков вариантов).
-
-// if (a / 100 > 0 && a / 1000 == 0)
+// if (a / 100 > 0 && a / 1000 == 0) - то число трехзначное
 
 // int Check(int a)
 // {
@@ -38,22 +40,50 @@ if (a / 100 > 0 && a / 1000 == 0)
 // }
 // Console.WriteLine("Вы ввели не трехзначное число. Введите число из трех знаков");
 
-
-// if (Num > 999 || Num < 100)
-//         Console.WriteLine("Вы ввели не трехзначное число. Введите число из трех знаков");
-
-
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
+/*
+int ThirdDigit(int Num)
+{
+    int result = -1;
+    if (Num >= 100)
+    {
+        while (Num > 999)
+        {
+            Num = Num / 10;
+        }
+        result = Num % 10;
+    }
+    return result;
+}
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int Figure = ThirdDigit(num);
+Console.WriteLine(Figure);
 */
-int ThirdDigit (int num)
 
-
-
-
-
+// код ниже не работает. пыталась впихнуть в одно условие (цикл)
+// while(num / 1000 !=0)
+//     num = num / 10;
+// int third = num % 10;
+// Console.WriteLine(third);
 
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+bool CheckWeekDay (int a)
+{
+    if(a == 6 || a == 7)
+        return true;
+    else
+        return false;
+}
+
+Console.WriteLine("Input a number of the week day: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool result = CheckWeekDay(num);
+Console.WriteLine("День является выходным? - " + result);
+
+// Решение без метода представляется мне более очевидным и красивым. Тогда можно консоль попросить написать однозначно - Деня является выходным или День не является выходным. 
+// Как тут сделать красивее, я не догадалась.
 
 */
