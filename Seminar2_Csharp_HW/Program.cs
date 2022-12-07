6f59f31da39401a43e276970bb217c5af5ff0e36
Darya_Kuptsova <darya.kuptsova.prof@gmail.com>
2022-12-07 18:31:40 +0300

    one more attempt with gitignore for 2nd seminar

--- Seminar2_Csharp_HW/Program.cs
+++ Seminar2_Csharp_HW/Program.cs
@@ -17,29 +17,6 @@ else
     Console.WriteLine("Вторая цифра этого числа: " + newnum);
     }
 
-
-// Попытки добавить проверку таким образом, чтобы снова запускалось сообщение о необходимости ввода числа, не увенчались успехом.
-// Ниже оставила свои варианты (это не одна программа, а несколько кусочков вариантов).
-// if (a / 100 > 0 && a / 1000 == 0) - то число трехзначное
-
-// int Check(int a)
-// {
-//     if (a > 999 || a < 100)
-//         Console.WriteLine("Вы ввели не трехзначное число. Введите число из трех знаков");
-//     return a;
-// }
-// int result = Check(num);
-// Console.WriteLine(result);
-
-// while (num < 1000 && num > 99)
-// {
-//     Console.WriteLine("Enter a three digit number: ");
-//     num = Convert.ToInt32(Console.ReadLine());
-//     int newnum = SecondDigit(num);
-//     Console.WriteLine("Second number is: " + newnum);
-// }
-// Console.WriteLine("Вы ввели не трехзначное число. Введите число из трех знаков");
-
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 /*
 int ThirdDigit(int Num)
@@ -61,13 +38,6 @@ int Figure = ThirdDigit(num);
 Console.WriteLine(Figure);
 */
 
-// код ниже не работает. пыталась впихнуть в одно условие (цикл)
-// while(num / 1000 !=0)
-//     num = num / 10;
-// int third = num % 10;
-// Console.WriteLine(third);
-
-
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 /*
 bool CheckWeekDay (int a)
@@ -82,8 +52,5 @@ Console.WriteLine("Input a number of the week day: ");
 int num = Convert.ToInt32(Console.ReadLine());
 bool result = CheckWeekDay(num);
 Console.WriteLine("День является выходным? - " + result);
-
-// Решение без метода представляется мне более очевидным и красивым. Тогда можно консоль попросить написать однозначно - Деня является выходным или День не является выходным. 
-// Как тут сделать красивее, я не догадалась.
 
 */
